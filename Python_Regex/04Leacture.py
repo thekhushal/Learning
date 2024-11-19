2d57206f27e74ab82783c233dbfd9955f14d595e
root <root@Khushal>
2024-11-19 13:38:57 +0530

    Updated

--- Python_Regex/04Leacture.py
+++ Python_Regex/04Leacture.py
@@ -1,10 +1,19 @@
-a = {'brand':'kia', 'Model':'Celtos','year':2023}
-print("Before:\n",a)
+# a = {'brand':'kia', 'Model':'Celtos','year':2023}
+# print("Before:\n",a)
 
-# One
-# a.popitem()
-# print(a)
+# # One
+# # a.popitem()
+# # print(a)
 
-# Two
-a.clear()
-print("After:\n",a)
+# # Two
+# a.clear()
+# print("After:\n",a)
+
+x = lambda a : a + 5
+print("total value = ", x(2))
+
+x = lambda a,b : a*b
+print("Total product = ", x(4, 5))
+
+x = lambda a,b,c : a+b-c
+print("  ")
