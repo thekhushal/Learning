fd9b468ad13489426f593d6309998adbc1a18392
thekhushal <derkhush@gmail.com>
2024-11-20 10:42:26 +0530

    Learning NumPy

---
+++ Python_Regex/06NumPy.py
@@ -0,0 +1,87 @@
+import numpy as np
+
+# a = np.random.randint(1,10,3)
+# print(a)
+
+# a = np.random.rand(5)
+# print(a)
+
+# a = np.random.randn(5)
+# print(a)
+
+# a = np.random.randint(1,50,12)
+# print(a,"\n")
+# a = a.reshape(6,2)
+# print(a)
+
+# a = a.reshape(-1, 4) #when one of the parameter is given -1 to the function, total number of elements are divided by other par and arr are formed in respective format 
+# print(a)
+
+# np.random.seed(333)
+# a = np.random.randint(1, 100, 10)
+# print(a)
+
+# a = np.array([1,2,3,4,5,6,7,8,9,0])
+# print(a)
+
+# b = a[3:6]
+# b[:] = 0
+# print(a)
+
+# b = a[3:6].copy()
+# b[:] = 0
+# print(a)
+# print(b)
+
+# a = np.arange(1,12)
+
+# # a = a>10
+# # print(a)
+
+# b = a > 10
+# a = a[b]
+# print(a)
+# print(b)
+
+# a = np.arange(1,5)
+# print(a)
+# print(a*2)
+
+# a = np.array([1,2,3,4]).reshape(2,2)
+# print(a)
+
+# b = np.array([5,6,7,8]).reshape(2,2)
+# print(b)
+
+# print(a + b)
+
+# print(a*b)
+
+# a = a.dot(b) # for matrix multiplication
+# print(a)
+
+# a = np.array([20,10,40,30,50])
+
+# # print(np.min(a))
+
+# # print(np.max(a))
+
+# # print(np.argmin(a))
+
+# # print(np.argmax(a))
+
+# print(np.sqrt(a))
+
+# a = np.linspace(1,2,5)
+# print(a)
+
+# a = np.array([10, 20, 30, 40, 50, 10, 20, 50])
+
+# print(np.unique(a, return_index = True, return_counts = True))
+
+# a = np.array([1,2,3,4])
+# b = np.array([5,6,7,8])
+# c = np.array([9, 10 , 11, 12])
+
+# print(np.hstack((a,b,c)))
+# print(np.vstack((a,b,c)))
