5aaa3dc5d39c12332d28711b153fe4fa0f4c8db3
thekhushal <derkhush@gmail.com>
2024-11-26 16:40:52 +0530

    Update

---
+++ Python_Regex/12Leacture.py
@@ -0,0 +1,3 @@
+# string tupple are immutable data type, if we do any changes these changes are made in new memory address
+# im-mutable datatype means, when value of variable of this tyoe is changed a new memory address is created
+
