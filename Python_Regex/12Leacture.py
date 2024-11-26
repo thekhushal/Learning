0ba998fe82f652df8e06b49789c68c760b8983d5
root <root@Khushal>
2024-11-26 17:02:39 +0530

    Learning Dictionaries

--- Python_Regex/12Leacture.py
+++ Python_Regex/12Leacture.py
@@ -1,3 +1,10 @@
 # string tupple are immutable data type, if we do any changes these changes are made in new memory address
 # im-mutable datatype means, when value of variable of this tyoe is changed a new memory address is created
 
+# dictionary
+
+# keys in dictionary should be onf immutable datatype
+mydictionary = {"amount":0}
+for x in range(10):
+    mydictionary["amount"] = mydictionary["amount"] +1
+print(mydictionary)
