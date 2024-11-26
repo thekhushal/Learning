eff18dab44c4809a2a0b1b12face602da32fda4a
root <root@Khushal>
2024-11-26 17:51:21 +0530

    new 12 lecture

--- Python_Regex/12Leacture.py
+++ Python_Regex/12Leacture.py
@@ -4,7 +4,42 @@
 # dictionary
 
 # keys in dictionary should be onf immutable datatype
-mydictionary = {"amount":0}
-for x in range(10):
-    mydictionary["amount"] = mydictionary["amount"] +1
-print(mydictionary)
+
+
+
+# mydictionary = {"amount":0}
+# for x in range(10):
+#     mydictionary["amount"] = mydictionary["amount"] +1
+# print(mydictionary)
+
+# a = "hello"
+# count_a = 0
+# for i in a:
+#     count_a+=1
+# print(count_a)
+
+# mydictionary = {"vouels" : 0}
+# a = "aeiou asdi asdfieo oaiudjfioiweio"
+# for char in a:
+#     if char in "a, e, i, o, u":
+#         mydictionary["vouels"] = mydictionary["vouels"]+1
+#         print(char)
+
+# print(mydictionary)
+
+# mydictionary ={}
+# name = "Khushal Sharma"
+
+# for char in name:
+#     if char not in mydictionary:
+#         mydictionary[char] = 1 # 'k' = 1 'h' = 1
+#     elif char in mydictionary:
+#         mydictionary[char] = mydictionary[char] + 1
+# print(mydictionary)
+
+
+
+# valid anagram
+mydictionary = {}
+
+name = "khushal"
