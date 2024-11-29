fd869e2d8a8a0e689347022dfe17a8abe1e531a5
root <root@Khushal>
2024-11-29 17:45:56 +0530

    Leet Code Problems

---
+++ Python_Regex/13Leacture.py
@@ -0,0 +1,30 @@
+# Difference method in python 
+# Intersection update 
+# Isomorphic string
+# a = 1, e = 5
+# g = 7, d = 4
+
+#  this solution is complete
+
+# s = "hva"
+# t = "rat"
+# iso = True
+# dict_s = {}
+# dict_t = {}
+
+# if len(s) == len(t):
+#     for index in range (len(s)):
+#         if (s[index] in dict_s) and (dict_s[s[index]] != t[index]):
+#             iso = False
+#         elif (t[index] in dict_t) and (dict_t[t[index]] != s[index]):
+#             iso = False
+#         else:
+#             dict_s[s[index]] = t[index]
+#             dict_t[t[index]] = s[index]
+# else:
+#     iso = False
+
+# if iso == True:
+#     print("It is isomorphic")
+# else:
+#     print("It is not isomorphic")
