9d323041b3bb3d61d8766dd45ac6aeabd8eb2781
root <root@Khushal>
2024-12-04 06:25:18 +0530

    Updated

---
+++ PythonRegEx/03LeetCode/02Sorting.py
@@ -0,0 +1,14 @@
+# sorting
+# a = [400, 32, 12, 40, 50, 23, 54, 256, 23, 67]
+
+# for x in range(len(a)):
+#     min = a[x]
+#     for y in range(x+1, len(a)):
+#         if min > a[y]:
+#             min = a[y]
+#             why = y
+#     swap = a [x]
+#     a [x] = min
+#     a[why] = swap
+# print(a)
+
---
+++ PythonRegEx/03LeetCode/11ThreeSumProblem.py
@@ -0,0 +1,21 @@
+# 3 Sum Problem
+nums = [-4, -1, -1, 0, 1, 2]
+
+# for x in range(len(nums) - 2):
+#     y = x + 1
+#     z = len(nums) - 1
+    
+#     while (y < z):
+#         if (nums[x] + nums[y] + nums[z] == 0):
+#             print(x, y, z)
+#             y += 1
+#             continue
+#         elif (nums[x] + nums[y] + nums[z] >= 0):
+#             z -= 1
+#         elif (nums[x] + nums[y] + nums[z] <= 0):
+#             y += 1
+
+# high order function
+# First class functionw
+
+    
