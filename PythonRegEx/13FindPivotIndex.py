a2ea91dc796cdb31f725255c22eec9327b0dc984
root <root@Khushal>
2024-12-16 04:01:39 +0530

    Update

--- PythonRegEx/13FindPivotIndex.py
+++ PythonRegEx/13FindPivotIndex.py
@@ -21,6 +21,7 @@ nums = [1,7,3,6,5,6]
 # nums = [1,2,3,4,5]
 print(PivotIndex(nums))
 
+# Problem description
 # Given an array of integers nums, calculate the pivot index of this array.
 # The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
 # If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
