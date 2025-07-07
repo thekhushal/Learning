0f00491b3d5da5f394c04b9e3b75ace17f21b521
thekhushal <derkhush@gmail.com>
2025-07-07 23:49:43 +0530

    Starting numpy

---
+++ Python/15--Function/09LambdaFunction.py
@@ -0,0 +1,9 @@
+# Lambda function
+
+def square1 (x):
+    return x**2
+
+square2 = lambda x : x ** 2
+
+print(square1(4))
+print(square2(4))
---
+++ Python/9005--NewProblems/try.py
@@ -0,0 +1,15 @@
+# s = "hello world"
+# print(''.join(str(ord(c)) for c in s))
+
+# s = 'Khushal Sharma'
+# print('.'.join(str(ord(c)) for c in s))
+
+s = [100,100,116,105,117,121]
+v = ['a', 'e', 'i', 'o', 'u']
+# for i in range(len(s)):
+#     if chr(s[i]) in v:
+#         s[i] = chr(s[i])
+print(s)
+
+
+print(chr(s[i]) for i in range(len(s)) if chr(s[i]) in )
