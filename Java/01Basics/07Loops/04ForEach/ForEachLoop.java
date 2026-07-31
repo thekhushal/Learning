bb572790512cf037225e9d1d056cabf2ebd9cd38
thekhushal <derkhush@gmail.com>
2026-07-31 18:03:04 +0530

    Learned For Each loop of java

---
+++ Java/01Basics/07Loops/04ForEach/ForEachLoop.java
@@ -0,0 +1,10 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] numbers = {23, 24, 46, 56, 67};
+
+        for(int number : numbers){
+            System.out.println(number);
+        }
+        // It's basically for each a in arr implement the following logic
+    }
+}
