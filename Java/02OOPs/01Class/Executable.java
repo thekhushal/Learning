fbc0862ac64a1d5c8003830b0f75a23c7cac41ef
thekhushal <derkhush@gmail.com>
2026-08-03 15:11:50 +0530

    Updated

--- Java/02OOPs/01Class/Executable.java
+++ Java/02OOPs/01Class/Executable.java
@@ -3,8 +3,8 @@ class Executable {
         Main myObj1 = new Main(); // object 1
         Main myObj2 = new Main(); // onject 2
  
-        System.out.println(myObj1.x);
-        System.out.println(myObj2.y + myObj2.x);
-        
+        System.out.println("Calling the class Main and accessing it through an object: "+myObj1.x);
+        System.out.println("Adding the two attributes of class Main: "+myObj2.y + myObj2.x);
+        // Main myObj3 = new Main(myObj2);
     }
 }
