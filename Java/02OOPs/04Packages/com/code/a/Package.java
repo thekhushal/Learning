1cc5ec3c5d1a3a730fc5206a61e0aa1ce55f636f
thekhushal <derkhush@gmail.com>
2026-08-04 10:50:51 +0530

    Importing methods within a package: learned different techniques and scenario

---
+++ Java/02OOPs/04Packages/com/code/a/Package.java
@@ -0,0 +1,7 @@
+package com.code.a;
+
+public class Package {
+    public static void M(){
+        System.out.println("Fuck it works");
+    }
+}
---
+++ Java/02OOPs/04Packages/com/code/b/Main.java
@@ -0,0 +1,7 @@
+package com.code.b;
+import static com.code.a.Package.M;
+public class Main {
+    public static void main(String[] args) {
+        M();
+    }
+}
