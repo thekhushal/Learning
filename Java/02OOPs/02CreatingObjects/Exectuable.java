e60b4cb6f4a82b4b138b2bdd6bb8561b1e40aa89
thekhushal <derkhush@gmail.com>
2026-08-05 14:58:04 +0530

    Updated

--- Java/02OOPs/02CreatingObjects/Exectuable.java
+++ Java/02OOPs/02CreatingObjects/Exectuable.java
@@ -14,16 +14,17 @@ public class Exectuable {
         // System.out.println("Rollno: " + Khushal.rno + "\n Name: " + Khushal.name + "\n Marks: " + Khushal.marks);
         
         // 2. Created 5 object and printed them out
-        // data[0] = new Student(01, "Khushal", 99);
-        // data[1] = new Student(02, "rahul", 97);
-        // data[2] = new Student(03, "keshav", 98);
-        // data[3] = new Student(04, "Diksha", 100);
-        // data[4] = new Student(05, "Indra", 66);
+        Student[] data = new Student[5];
+        data[0] = new Student(01, "Khushal", 99);
+        data[1] = new Student(02, "rahul", 97);
+        data[2] = new Student(03, "keshav", 98);
+        data[3] = new Student(04, "Diksha", 100);
+        data[4] = new Student(05, "Indra", 66);
 
-        // for(int i = 0; i < data.length; i++ ){
+        for(int i = 0; i < data.length; i++ ){
 
-        //     System.out.println("Rollno: " + data[i].rno + "\n\tName: " + data[i].name + "\n\tMarks: " + data[i].marks);
-        // }
+            System.out.println("Rollno: " + data[i].rno + "\n\tName: " + data[i].name + "\n\tMarks: " + data[i].marks);
+        }
 
 
         // Created several objects based on the data in the csv
