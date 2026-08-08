54c77aed09175ddd374279f55c7f6a67f255e14b
thekhushal <derkhush@gmail.com>
2026-08-08 23:53:16 +0530

    Hierarchical Inheritance: Explored scenarios where hierarchical inheritance makes sence

---
+++ Java/02OOPs/09Inheritance/com/hierarchicalInheritance/Main.java
@@ -0,0 +1,16 @@
+package com.hierarchicalInheritance;
+
+class Animal{
+    void category(){
+        System.out.println("Vertebor");
+    }
+}
+
+class Dog extends Animal{
+    void features(){
+        
+    }
+}
+public class Main {
+    
+}
