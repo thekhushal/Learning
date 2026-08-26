aab69e5fd3a84fb29651cf6bd66f548bbb07f474
thekhushal <derkhush@gmail.com>
2026-08-26 17:45:43 +0530

    it's controller

---
+++ Java/demo/demo/src/main/java/com/example/demoapplication/UserController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+}
