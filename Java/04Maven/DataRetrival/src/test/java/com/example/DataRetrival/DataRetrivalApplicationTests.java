7bb867e8ac15ac196cd7c92dd2d0a8eeb0a06fb2
thekhushal <derkhush@gmail.com>
2026-09-07 17:33:49 +0530

    Comiting the entire project

---
+++ Java/04Maven/DataRetrival/src/test/java/com/example/DataRetrival/DataRetrivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DataRetrival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataRetrivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
