fef75546073397df44e36d5651bfb1b8350b12e2
thekhushal <derkhush@gmail.com>
2026-09-07 17:26:07 +0530

    Application

---
+++ Java/04Maven/DataRetrival/src/main/java/com/example/DataRetrival/DataRetrivalApplication.java
@@ -0,0 +1,13 @@
+package com.example.DataRetrival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRetrivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRetrivalApplication.class, args);
+	}
+
+}
