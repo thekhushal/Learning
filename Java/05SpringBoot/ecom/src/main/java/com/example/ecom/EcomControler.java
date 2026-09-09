0e2a5ac3197954807bd8d8476f688c922dd1e6a8
thekhushal <derkhush@gmail.com>
2026-09-09 18:56:33 +0530

    Created a controller

---
+++ Java/05SpringBoot/ecom/src/main/java/com/example/ecom/EcomControler.java
@@ -0,0 +1,53 @@
+package com.example.ecom;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class EcomControler {
+
+    // DI
+    EcomService service;
+    public EcomControler(EcomService service){
+        this.service = service;
+    }
+
+    // Creating Products
+    @PostMapping("/product")
+    public String createProduct(@RequestBody Product product){
+        return service.createProduct(product);
+    }
+
+    // Retriving All Products
+    @GetMapping(value = "/product")
+    public List<Product> getProducts(){
+        return service.getProducts();
+    }
+
+    // Get Product by id
+    @GetMapping ("/product/{id}")
+    public Product getProduct(@PathVariable int id){
+        return service.getProduct(id);
+    }
+
+    // Get product by name
+    @GetMapping(value = "/product", params = "name")
+    public Product geProduct(@RequestParam String name){
+        return service.getProductByName(name);
+    }
+
+    // get product by Combination (name and id)
+    @GetMapping (value = "/product/{id}", params = "name")
+    public Product getProductByCombination(@PathVariable int id, 
+        @RequestParam String name){
+            return service.getProductByCombination(id, name);
+        }
+
+    
+}
