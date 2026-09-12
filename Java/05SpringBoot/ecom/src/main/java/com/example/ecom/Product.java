ab3f707d2fc490c52113f1c3c520628b10636214
thekhushal <derkhush@gmail.com>
2026-09-12 14:34:46 +0530

    Implimented validation

--- Java/05SpringBoot/ecom/src/main/java/com/example/ecom/Product.java
+++ Java/05SpringBoot/ecom/src/main/java/com/example/ecom/Product.java
@@ -1,9 +1,22 @@
 package com.example.ecom;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class Product {
     private int id;
+
+    @NotBlank
     private String name;
+
+    @NotBlank 
     private String category;
+
+    @NotNull 
+    @Positive 
+    @Size (min=500, max = 5000)
     private Integer price;
 
     public Product(String name, String category, Integer price){
